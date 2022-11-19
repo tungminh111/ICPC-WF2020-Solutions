124337eb8794f3ac34f3be916b2c1d953c3e190f
tungminh111 <37438284+tungminh111@users.noreply.github.com>
2022-11-19 22:46:01 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,95 +1,57 @@
-#include "bits/stdc++.h"
-#define For(i, a, b) for(int i = a; i <= b; i++)
-#define Ford(i, a, b) for(int i = a; i >= b; i--)
- 
+#include <bits/stdc++.h>
+
 using namespace std;
-const int N = 2e5 + 1067, M = 1e5 + 1067;
-int t;
-long long r, c;
-
-pair<int, int> getRC(long long pos) {
-    int row = (pos - 1) / c + 1;
-    int col = pos - c * (row - 1);
-    return make_pair(row, col);
-}
-
-int getStep(int p) {
-    int lo = 1;
-    int hi = r;
-    int result = 0;
-    long long startPos = (p - 1) * r + 1;
-    while (lo != hi) {
-        pair<int, int> startRC = getRC(startPos + lo - 1); 
-        pair<int, int> endRC = getRC(startPos + hi - 1); 
-        if (lo == startRC.first && hi == endRC.first) break;
-        result += 1;
-        lo = startRC.first; hi = endRC.first;
-    }
-    return result + 1;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);;
+    int r, c;
+    cin >> r >> c;
 
-    // cin >> t;
-    t = 1;
-    For(test, 1, t) {
-        cin >> r >> c;
-        vector<pair<int, int> > centerPos;
-        if (r % 2) {
-            if (c % 2) {
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2 + 1));
-            } else {
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2));
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2 + 1));
-            }
-        } else {
-            if (c % 2) {
-                centerPos.push_back(make_pair(r / 2, c / 2 + 1));
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2 + 1));
-            } else {
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2));
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2 + 1));
-                centerPos.push_back(make_pair(r / 2, c / 2 + 1));
-                centerPos.push_back(make_pair(r / 2 + 1, c / 2));
+    vector<int> fret;
+    auto dis = [&r, &c](const int& i, const int& j) {
+        int center_i = r / 2 + (r&1 || i > r / 2 - 1) - 1;
+        int center_j = c / 2 + (c&1 || j > c / 2 - 1) - 1;
+        return abs(i - center_i) + abs(j - center_j);
+    };
+
+    for(int i = 0; i < c; ++i) {
+        long long lo = (long long)i * r;
+        long long hi = ((long long)i + 1) * r - 1;
+
+        int ret_i, ret_j;
+        long long ret_index;
+        ret_i = -1;
+        bool dup = false;
+        for(int j = lo / c; j <= hi / c; ++j) {
+            long long index = (long long)i * r + j;
+            int row = index / c;
+            if (row == j) {
+                if (ret_i != -1)
+                    dup = true;
+                ret_i = j;
+                ret_j = index % c;
+                ret_index = index;
             }
         }
-        int curRow = 1;
-        pair<int, int> finalRC = make_pair(-1, -1);
-        int finalDistance = r + c;
-        int finalP = -1;
-        int finalStep = -1;
-        For(p, 1, c) {
-            long long startPos = r * (p - 1) + 1;
-            while (true) {
-                long long curPos = startPos + curRow - 1;
-                pair<int, int> curRC = getRC(curPos);
-                // cout << curRow << " " << curPos << " " << curRC.first << endl;
-                if (curRC.first > curRow) curRow += 1; else break;
-            }
-            if (curRow <= r) {
-                pair<int, int> rc = getRC(startPos + curRow);
-                if (rc.first == curRow + 1) {
-                     continue; 
-                }
-            }
-            pair<int, int> rc = getRC(startPos + curRow - 1);
-            int distance = r + c;
-            for (auto pr: centerPos) {
-                distance = min(abs(rc.first- pr.first) + abs(rc.second - pr.second), distance);
-            }
-            if (distance < finalDistance) {
-                finalDistance = distance;
-                finalRC = rc; 
-                finalP = p;
-            }
+        if (dup) continue;
 
+        function<int(long long)> solve = [&solve, &ret_index, &i, &r, &c](const long long& index) {
+            if (index == ret_index)
+                return 0;
+            return solve((long long)i * r + index / c) + 1;
+        };
 
+        int ret = max(solve(lo) + (lo / c > 0), solve(hi) + (hi / c < r - 1));
+        if (fret.empty()
+            || dis(fret[1], fret[2]) > dis(ret_i, ret_j)
+            || dis(fret[1], fret[2]) == dis(ret_i, ret_j) && fret[0] > i) {
+            fret = {i, ret_i, ret_j, ret};
         }
-        finalStep = getStep(finalP); 
-        cout << finalP << " " << finalRC.first << " " << finalRC.second << " " << finalStep << endl;
     }
+    cout << fret[0] + 1 << " " << fret[1] + 1 << " " << fret[2] + 1 << " " << fret[3] << endl;
+
+    return 0;
 }
